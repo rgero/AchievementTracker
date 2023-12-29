12bc96a02ce023f19e9fc4e70576194bfd0bd8ee
Roy Gero <roy.gero.ii@gmail.com>
2023-12-29 10:33:36 -0500

    Adding a simple landing page with buttons to navigate to the Sign Up and Login pages

--- achievement-tracker/src/pages/LandingPage.jsx
+++ achievement-tracker/src/pages/LandingPage.jsx
@@ -1,4 +1,7 @@
+import Button from "../styles/Button";
+import Card from "../ui/Card";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom"
 
 const StyledLandingPage = styled.div`
   height: 100vh;
@@ -8,11 +11,25 @@ const StyledLandingPage = styled.div`
   justify-content: center;
 `;
 
+const StyledButton = styled(Button)`
+  margin: 1rem;
+`
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
     <StyledLandingPage>
-      Welcome!
+      <Card>
+        <Card.Header>The Achievement Tracker</Card.Header>
+        <Card.Body>
+          Welcome to the Achievement Tracker. <br/>A website meant for you to easily remember your accomplishements.
+        </Card.Body>
+        <Card.Footer>
+          <StyledButton onClick={()=> navigate('/signup')}>Create new account</StyledButton>
+          <StyledButton onClick={()=> navigate('/login')}>Login</StyledButton>
+        </Card.Footer>
+      </Card>
+
     </StyledLandingPage>
   )
 }
