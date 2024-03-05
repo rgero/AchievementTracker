33404562913e6a3ac5be5523b7b01401a97f50f3
Roy Gero <roy.gero.ii@gmail.com>
2024-03-05 13:50:30 -0500

    Removing an unnecessary sort.
    I was already sorting the data through Supabase, I didn't need to sort it again.

--- achievement-tracker/src/features/achievements/AchievementTable.jsx
+++ achievement-tracker/src/features/achievements/AchievementTable.jsx
@@ -7,13 +7,10 @@ import Pagination from "../../ui/Pagination";
 import { Sizes } from "../../utils/constants";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
-import { isBefore } from "date-fns";
 import { useAchievements } from "./hooks/useAchievements";
-import { useSearchParams } from "react-router-dom";
 
 const AchievementTable = () => {
   const { isLoading, achievements, count } = useAchievements();
-  const [searchParams] = useSearchParams();
   
   const [isDesktop, setDesktop] = useState(window.innerWidth > Sizes.minScreenSize);
   const updateMedia = () => {
@@ -26,30 +23,6 @@ const AchievementTable = () => {
 
   if (isLoading) return <Spinner />;
   if (achievements.length === 0) return <Empty resourceName="bookings"/>
-
-  // Sort the achievements
-  const sortBy = searchParams.get('sortBy') || 'name-asc';
-  const [field, direction] = sortBy.split('-');
-  const modifier = direction === 'asc' ? 1 : -1;
-
-  // Need to sort these better.
-  let sortedAchievements = achievements;
-
-  switch(field)
-  {
-    case "name":
-      sortedAchievements = achievements?.sort((a, b) => a.name.localeCompare(b.name) * modifier);
-      break;
-    case "date":
-      sortedAchievements = achievements?.sort((a, b) => {
-        return isBefore(new Date(a), new Date(b)) * modifier;
-      });
-      break;     
-    default:
-      sortedAchievements = achievements?.sort((a, b) => a[field] - b[field] * modifier);
-      break;
-  }
-  
 
   let columns = '4fr 1fr 0.25fr 0.25fr';
   if (!isDesktop) {
@@ -67,7 +40,7 @@ const AchievementTable = () => {
           </>}
         </Table.Header>
         <Table.Body 
-          data={sortedAchievements} 
+          data={achievements} 
           render={(achievement) => <AchievementRow achievement={achievement} key={achievement.id} />} 
         />
         <Table.Footer>
