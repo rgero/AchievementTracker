321513f1b5c2e7eb914f6cf9b21251d214198027
Roy Gero <roy.gero.ii@gmail.com>
2024-04-18 16:38:12 -0400

    Fixing the Not Found page a bit.

--- achievement-tracker/src/pages/PageNotFound.jsx
+++ achievement-tracker/src/pages/PageNotFound.jsx
@@ -1,17 +1,27 @@
+import Button from "../styles/Button";
+import Heading from "../ui/Heading";
+import Row from "../styles/Row";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const StyledPageNotFound = styled.div`
   height: 100vh;
   background-color: var(--color-grey-50);
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
       <StyledPageNotFound>
-          Page has not been found fool.
+        <Heading as="h1">Page not found</Heading>
+        <Row>
+          The Page you are looking for has not been found.
+        </Row>
+        <Row><Button onClick={()=> navigate("/")}>Click here to return</Button></Row>
       </StyledPageNotFound>
   )
 }
