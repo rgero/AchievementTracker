d9574dcbd3b9ba971b6bc810b2b54c05ac11309b
Roy Gero <roy.gero.ii@gmail.com>
2025-04-19 21:28:50 -0400

    Merging the folders.
    There's no reason to keep the old one when I can look at the main branch. I had it around originally to make referencing easier but I never use it.

--- achievement-tracker/src/features/stats/hooks/useStatsAchievements.js
+++
@@ -1,9 +0,0 @@
-import { getStatsAchievements } from "../../../services/apiAchievements";
-import { useQuery } from "@tanstack/react-query";
-
-export const useStatsAchievements = () => {
-
-  // This is cool, the filter input makes it a dependency like useEffect.
-  const {isLoading, data: {data: achievements, count} = {}, error} = useQuery({queryKey: ["statsAchievements"], queryFn: () => getStatsAchievements()});
-  return { isLoading, error, achievements, count };
-}
--- achievement-tracker/src/pages/ViewAchievementPage.jsx
+++
@@ -1,9 +0,0 @@
-import AchievementDetail from "../features/achievements/AchievementDetail"
-
-const ViewAchievementPage = () => {
-  return (
-    <AchievementDetail/>
-  )
-}
-
-export default ViewAchievementPage
