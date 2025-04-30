922c17304de64d619f2bf3fccd545efc0df816f0
Roy Gero <roy.gero.ii@gmail.com>
2025-04-29 22:36:10 -0400

    Changing the styling on the settings dialog.
    Still need to figure out what else to put here.

--- achievement-tracker/src/components/settings/SettingsContent.tsx
+++ achievement-tracker/src/components/settings/SettingsContent.tsx
@@ -1,17 +1,13 @@
-import { Grid2 as Grid, Typography } from "@mui/material"
+import { Container, Typography } from "@mui/material"
 
 import DarkModeToggle from "./options/DarkModeToggle"
 
 const SettingsContent = () => {
   return (
-    <Grid container sx={{alignItems: "center", justifyContent: "space-between"}}>
-      <Grid>
-        <Typography variant="body1" sx={{fontWeight: "bold"}}>General Settings</Typography>
-      </Grid>
-      <Grid>
-        <DarkModeToggle/>
-      </Grid>
-    </Grid>
+    <Container>
+      <Typography variant="body1" sx={{fontWeight: "bold"}}>General Settings</Typography>
+      <DarkModeToggle/>
+    </Container>
   )
 }
 
